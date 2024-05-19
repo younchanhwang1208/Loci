89221482c401e562c40ac35998b6ea79e796302e
Younchan Hwang <younchanhwang1208@gmail.com>
2024-05-18 23:08:57 -0700

    repositioned map

--- Loci/ImmersiveView.swift
+++ Loci/ImmersiveView.swift
@@ -10,20 +10,17 @@ import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
-    var body: some View {
-        //have to reposition them (it is currently below the ground)
-        Model3D(named: "amongus") { model in
-            model
-            //.resizable()
-            .aspectRatio(contentMode: .fill)
-        } placeholder: {
-            ProgressView()
+    func setPosition(view: UIView, x: CGFloat, y: CGFloat) {
+            var frame = view.frame
+            frame.origin = CGPoint(x: x, y: y)
+            view.frame = frame
         }
+    var body: some View {
         RealityView { content in
             // Add the initial RealityKit content
             if let immersiveContentEntity = try? await Entity(named: "amongus", in: realityKitContentBundle) {
                 content.add(immersiveContentEntity)
-
+                immersiveContentEntity.position = [0, 1, 0]
                 // Add an ImageBasedLight for the immersive content
                 guard let resource = try? await EnvironmentResource(named: "ImageBasedLight") else { return }
                 let iblComponent = ImageBasedLightComponent(source: .single(resource), intensityExponent: 0.25)
