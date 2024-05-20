159b15f4b107d5ce99a5969f2d1e54159417d69a
Younchan Hwang <younchanhwang1208@gmail.com>
2024-05-20 01:50:00 -0700

    added comments for Shane; added collision shapes to prevent user from passing through the map

--- Loci/ImmersiveView.swift
+++ Loci/ImmersiveView.swift
@@ -18,6 +18,8 @@ struct ImmersiveView: View {
                 immersiveContentEntity.position = [25, -5, -50] // Shifted right and forward
                 immersiveContentEntity.scale = [1, 1, 1] // Scaled down by a factor of 0.5
                 immersiveContentEntity.orientation = simd_quatf(angle: .pi, axis: [0, 1, 0]) // Rotate 180 degrees around the Y axis
+                //allow collision for touch (also) -> if it doesn't work just remove
+                immersiveContentEntity.generateCollisionShapes(recursive: true)
                 
                 content.add(immersiveContentEntity)
                 // Add an ImageBasedLight for the immersive content
@@ -25,25 +27,36 @@ struct ImmersiveView: View {
                 let iblComponent = ImageBasedLightComponent(source: .single(resource), intensityExponent: 0.25)
                 immersiveContentEntity.components.set(iblComponent)
                 immersiveContentEntity.components.set(ImageBasedLightReceiverComponent(imageBasedLight: immersiveContentEntity))
-
-                // Put skybox here.  See example in World project available at
-                // https://developer.apple.com/
             }
+            
+            //TO SHANE: 
+            //0. Check if the Entity() function is working for plane, tea, doll, and banana (you might have to put them in realitykitcontentbundle jsut like we did for amongus map
+            //1. reposition/scale them so that it's visible
+            //2. possible film a demo? (or we can work on this together)
+            //After our deliverable, I'm adding a feature where if we tap on it (or any gesture), the object disappears
             if let plane = try? await Entity(named: "plane" /*in: realityKitContentBundle*/) {
                 plane.position = [25, -5, -50] // Shifted right and forward
                 content.add(plane)
+                //allow collision for touch-> if it doesn't work just remove
+                plane.generateCollisionShapes(recursive: true)
             }
             if let tea = try? await Entity(named: "tea" /*in: realityKitContentBundle*/) {
                 tea.position = [25, -5, -50] // Shifted right and forward
                 content.add(tea)
+                //allow collision for touch-> if it doesn't work just remove
+                tea.generateCollisionShapes(recursive: true)
             }
             if let doll = try? await Entity(named: "doll" /*in: realityKitContentBundle*/) {
                 doll.position = [25, -5, -50] // Shifted right and forward
                 content.add(doll)
+                //allow collision for touch-> if it doesn't work just remove
+                doll.generateCollisionShapes(recursive: true)
             }
             if let banana = try? await Entity(named: "banana" /*in: realityKitContentBundle*/) {
                 banana.position = [25, -5, -50] // Shifted right and forward
                 content.add(banana)
+                //allow collision for touch-> if it doesn't work just remove
+                banana.generateCollisionShapes(recursive: true)
             }
         }
     }
