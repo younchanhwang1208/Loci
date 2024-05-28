d901d30773c5a9f7a3e0aa2fdc89f57b44eea65f
shanemion <smion@stanford.edu>
2024-05-28 01:59:45 -0700

    Added basic hoop map navigation successfully. Attempted to use .move instead of .setPosition for smooth transitions, but don't know what to put for relative position.

--- Loci/Helper/ImmersiveViewModel.swift
+++ Loci/Helper/ImmersiveViewModel.swift
@@ -4,6 +4,7 @@ import SwiftUI
 import RealityKitContent
 
 @MainActor class ImmersiveViewModel: ObservableObject {
+    var initialMap: Entity?
     var camEntity: AnchorEntity = {
         let camPov = AnchorEntity()
         camPov.position = [0, 0, 0]
@@ -12,28 +13,48 @@ import RealityKitContent
     
     func setupTapGesture(for hoop: Entity) {
         // Add a collision component to the hoop if it doesn't already have one
-        if hoop.components[CollisionComponent.self] == nil {
-            let collisionComponent = CollisionComponent(shapes: [ShapeResource.generateBox(width: 2.0, height: 2.0, depth: 0.02)])
+//        if hoop.components[CollisionComponent.self] == nil {
+            let collisionComponent = CollisionComponent(shapes: [ShapeResource.generateBox(width: 0.5 / 9, height: 0.2 / 9, depth: 0.5/9)])
             hoop.components.set(collisionComponent)
             hoop.components.set(InputTargetComponent())
             print("Added collision and input target components to hoop: \(hoop.name)")
         }
-        }
+//    }
     
     var hoopTapGesture: some Gesture {
-        SpatialTapGesture().targetedToAnyEntity().onEnded { [weak self] value in
-            guard let self = self else {
-                print("Self is nil, exiting closure")
-                return
+        SpatialTapGesture()
+            .targetedToAnyEntity()
+            .onEnded { [weak self] value in
+                guard let self = self, let initialMap = self.initialMap else {
+                    print("Self or initialMap is nil, exiting closure")
+                    return
+                }
+                print("Tapped on entity: \(value.entity.name)")
+                self.moveEntitiesRelativeToHoop(hoop: value.entity, in: initialMap)
             }
-            print("Tapped on entity: \(value.entity.name)")
-            self.moveToHoop(hoop: value.entity)
-        }
     }
     
-    private func moveToHoop(hoop: Entity) {
+    private func moveEntitiesRelativeToHoop(hoop: Entity, in map: Entity) {
         let hoopPosition = hoop.position(relativeTo: nil)
-        print("Moving camera to hoop position: \(hoopPosition)")
-        camEntity.setPosition(hoopPosition, relativeTo: nil)
+//        let offset = SIMD3<Float>(-hoopPosition.x, -hoopPosition.y, -hoopPosition.z)
+        let offset = SIMD3<Float>(-hoopPosition.x / 9, 0, -hoopPosition.z / 9)
+
+        print("Moving entities by offset: \(offset)")
+        
+        for child in map.children {
+            let currentPosition = child.position(relativeTo: nil)
+            let newPosition = currentPosition + offset
+            child.setPosition(newPosition, relativeTo: nil)
+            // buggy move line
+//            child.move(to: Transform(translation: newPosition), relativeTo: map, duration: 1.0)
+
+            print("Moved entity \(child.name) to new position: \(newPosition)")
+        }
+        
+        let mapCurrentPosition = map.position(relativeTo: nil)
+        let mapNewPosition = mapCurrentPosition + offset
+        map.setPosition(mapNewPosition, relativeTo: nil)
+//        map.move(to: Transform(translation: mapNewPosition), relativeTo: camEntity, duration: 1.0)
+        print("Moved map to new position: \(mapNewPosition)")
     }
 }
