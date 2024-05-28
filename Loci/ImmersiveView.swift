c7792e6add4cdc7595cb1aea58d242853f68375e
shanemion <smion@stanford.edu>
2024-05-28 14:41:10 -0700

    added comments for youn describing map translation logic and MVVM model, as well as some UI modeling

--- Loci/ImmersiveView.swift
+++ Loci/ImmersiveView.swift
@@ -3,6 +3,17 @@ import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
+    /* 
+     Following the "MVVM (Model-View-Model View)" Swift decoupling strategy.
+     
+     The struct ImmersiveView is the frontend stuff, where we load content into our RealityView. We first load map in, which is the "Whatever" entity in the RealityKitContent folder. The "Whatever" entity holds a child called "Root", and "Root" contains its own children, namely the "hoops_{i}" and the "amongus" usda's (The amongus usda is the map, so the naming scheme is kind of confusing but hopefully this comment cleared it up"
+     
+     The image based light is default, we might need to add it to Step View 2 to the hoop, because the hoop in the preview is not lit"
+     
+     The for loop looping through hoops is adding the collision components to each of the hoops, which basically just ensures that they are able to be pinched/tapped/selected by the user when clicked on after SpatialTapGesture is placed on the entire "Whatever" entity in line #49. The collision component code is executed by finding each of the hoops prerendered into the map, and calling the method defined in the VM component in "ImmersiveViewModel"
+     
+     
+    */
     @StateObject private var model = ImmersiveViewModel()
     
     var body: some View {
