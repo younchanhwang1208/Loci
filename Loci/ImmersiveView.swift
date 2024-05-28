e31cd6b798e886a473f7f7898ea48434b4fe2184
shanemion <smion@stanford.edu>
2024-05-28 01:59:45 -0700

    Added basic hoop map navigation successfully. Attempted to use .move instead of .setPosition for smooth transitions, but don't know what to put for relative position.

--- Loci/ImmersiveView.swift
+++ Loci/ImmersiveView.swift
@@ -10,11 +10,15 @@ struct ImmersiveView: View {
             // Load the initial RealityKit content from the USDA file
             if let map = try? await Entity(named: "Whatever", in: realityKitContentBundle) {
                 // Adjusted position, scale, and orientation
-                map.position = [0, 0, 0]
-                map.scale = [1, 1, 1]
+                map.position = [2, 0, 0]
+                map.scale = [9, 9, 9]
                 map.orientation = simd_quatf(angle: .pi, axis: [0, 1, 0]) // Rotate 180 degrees
                 
                 content.add(map)
+                
+                // set the scaleFactor in the model to adjust the offsets
+//                model.scaleFactor = map.scale
+
                 
                 // Add an ImageBasedLight for the immersive content
                 if let resource = try? await EnvironmentResource(named: "ImageBasedLight") {
@@ -24,21 +28,25 @@ struct ImmersiveView: View {
                 }
                 
                 // Add tap gestures to hoops
-                for i in 0...9 {
-                    let hoopName = i == 0 ? "hooptest" : "hooptest_\(i)"
+                for i in 0...16 {
+                    let hoopName = "hooptest_\(i)"
                     if let hoop = map.findEntity(named: hoopName) {
                         print("Setting up tap gesture for hoop: \(hoopName)")
                         model.setupTapGesture(for: hoop)
-                        print("Set up tap gesture for hoop: for hoop: \(hoopName)")
+                        print("Set up tap gesture for hoop: \(hoopName)")
                     } else {
                         print("Could not find hoop: \(hoopName)")
                     }
                 }
                 
-                // Add the camera entity to the content
-                content.add(model.camEntity)
+                // Store the initial map entity and its scale factor in the model
+                model.initialMap = map
+//                model.scaleFactor = map.scale
+            } else {
+                print("Could not load initial RealityKit content")
             }
-        }.gesture(model.hoopTapGesture)
+        }
+        .gesture(model.hoopTapGesture)
     }
 }
 
