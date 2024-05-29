17edff527b7d135ce3efaf5e6140d03c6f233285
Younchan Hwang <younchanhwang1208@gmail.com>
2024-05-29 03:09:25 -0700

    added transparent among us background to the 'full immersion' button pn MixedVsFull website. hovering effect doesn't seem to be working: you should check if this is just a problem with my macbook. Action item for tmrw would be to change more UI in other screens.

--- Loci/Instructions/MixedVsFull.swift
+++ Loci/Instructions/MixedVsFull.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct MixedVsFull: View {
+    @State private var isHovering = false
     var body: some View {
         VStack {
             Button("Mixed Reality") {
@@ -16,11 +17,25 @@ struct MixedVsFull: View {
             NavigationLink(destination: PlaceObjects(spaceTitle: "Among Us Immersive Space", imageName: "amongus_minimap")) {
                 Text("Full Immersion")
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(Color.blue)
+                    .background(
+                        Image("amongus_button")
+                            .resizable()
+                            .scaledToFill()
+                            .opacity(0.5)
+                            .onHover { hovering in
+                                print("hovering on")
+                                isHovering = hovering
+                            }
+                            .scaleEffect(isHovering ? 1.7 : 1.0)
+                            .overlay(Color.black.opacity(0.3))
+                            //.animation(.easeInOut(duration: 0.3), value: isHovering)
+                    )
                     .foregroundColor(.white)
                     .font(.title)
                     .cornerRadius(10)
+                    .hoverEffect(.lift)
                     .padding()
+
             }
         }
     }
