80fee46aaba958d3219013b68167db2d30001ef7
shanemion <smion@stanford.edu>
2024-06-01 13:38:19 -0700

    Added totem struct, added totemDictionary initialization, made totem's clickable, but currently I hide them all on load in

--- Loci/Instructions/MapNav.swift
+++ Loci/Instructions/MapNav.swift
@@ -3,40 +3,31 @@ import RealityKit
 
 struct MapNav: View {
     @State private var showImmersiveSpace = false
-
-    @Environment(\.openImmersiveSpace) var openImmersiveSpace
-    @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
+    @State private var showPanel = false
 
     var body: some View {
-        VStack {
-            Text("Map Navigation")
-                .font(.largeTitle)
-                .padding(.top, 50)
+        NavigationView {
+            VStack {
+                Text("Map Navigation")
+                    .font(.largeTitle)
+                    .padding(.top, 50)
 
-            Text("Toggle to open the Among Us Immersive Space.")
-                .padding(.bottom, 30)
+                Text("Toggle to open the Among Us Immersive Space.")
+                    .padding(.bottom, 30)
 
-            Toggle(showImmersiveSpace ? "Close Immersive Space" : "Open Immersive Space", isOn: $showImmersiveSpace)
-                .onChange(of: showImmersiveSpace) { _, isShowing in
-                    Task {
-                        if isShowing {
-                            switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                            case .opened:
-                                break
-                            case .error, .userCancelled:
-                                showImmersiveSpace = false
-                            @unknown default:
-                                showImmersiveSpace = false
-                            }
-                        } else {
-                            await dismissImmersiveSpace()
-                        }
+                Toggle(showImmersiveSpace ? "Close Immersive Space" : "Open Immersive Space", isOn: $showImmersiveSpace)
+                    .onChange(of: showImmersiveSpace) { _, isShowing in
+                        showPanel = isShowing
                     }
-                }
-                .toggleStyle(.button)
-                .padding()
+                    .toggleStyle(.button)
+                    .padding()
+                
+                    .navigationDestination(isPresented: $showPanel) {
+                        ImmersiveSpacePanel(showImmersiveSpace: $showImmersiveSpace)
+                    }
+            }
+            .padding(70)
         }
-        .padding(70)
     }
 }
 
