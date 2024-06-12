219b83316819dac143ce39d7769cdc2a462086e2
shanemion <smion@stanford.edu>
2024-06-12 04:03:33 -0700

    changed entities to holograms, added Loci entity to ContentView panel preview

--- Loci/ContentView.swift
+++ Loci/ContentView.swift
@@ -6,8 +6,24 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Model3D(named: "Scene", bundle: realityKitContentBundle)
-                    .padding(.bottom, 50)
+                RealityView { content in
+                    do {
+                        if let entity = try? await Entity(named: "Whatever", in: realityKitContentBundle) {
+                            entity.scale = [0.06, 0.06, 0.06]  // Scaling down by a factor of 100
+                            let tiltAngle = simd_quatf(angle: .pi / 4, axis: [0.8, 0, 0]) // 45 degrees
+                            entity.orientation = tiltAngle
+                            content.add(entity)
+                            
+                            if let resource = try? await EnvironmentResource(named: "ImageBasedLight") {
+                                let iblComponent = ImageBasedLightComponent(source: .single(resource), intensityExponent: 0.25)
+                                entity.components.set(iblComponent)
+                                entity.components.set(ImageBasedLightReceiverComponent(imageBasedLight: entity))
+                            }
+                        }
+                        
+                    }
+                }
+                .padding(.bottom, 20)
 
                 Text("Welcome to Loci: Memory Palace!").font(.largeTitle)
 
@@ -17,7 +33,7 @@ struct ContentView: View {
                         .padding()
                         .foregroundColor(.white)
                         .cornerRadius(10)
-                }
+                }.padding(.bottom, 50)
             }
         }
     }
